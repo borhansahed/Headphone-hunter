9b75b2d3dc45195537c0c65f433d962654f37c5e
wahi90 <mdshahed1098@gmail.com>
2022-04-05 01:38:17 +0600

    review done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import useReviews from '../../Hook/useReview';
+import ReviewItems from '../ReviewItems/ReviewItems';
 import './Review.css'
 
 const Review = () => {
+    const [reviews, setReviews] = useReviews();
     return (
-        <div className='all-reviews text-center'>
-           <h1 className='text-4xl font-black'>All Customer Reviews</h1> 
+        <div className='all-reviews-container'>
+            <div className='mt-10 text-center'>
+                  <h1 className='text-4xl font-black'>All Customer Reviews</h1>
+            </div>
+          <div className='all-reviews'>
+            {
+                reviews.map(review =><ReviewItems key={review._id} review={review}> </ReviewItems> )
+            }  
+            
+          </div>
         </div>
     );
 };
--- src/components/ReviewItems/ReviewItems.js
+++ src/components/ReviewItems/ReviewItems.js
@@ -1,5 +1,9 @@
+
 import React from 'react';
 import './ReviewItems.css';
+import { FaStar } from 'react-icons/fa';
+
+
 
 const ReviewItems = (props) => {
     const {name ,review , picture , ratings ,  }=props.review;
@@ -8,10 +12,10 @@ const ReviewItems = (props) => {
         
          <div >
            <div className='review-card hover:shadow-lg'>
-           <img className='rounded-xl ' src={picture} alt="" />
-            <h4 className='text-center'>{name}</h4>
-             <blockquote className='font-medium'>{review.slice(0,200)}</blockquote>
-            <h4>{ratings}</h4>
+           <img className='rounded-full ' src={picture} alt="" />
+            <h4 className='text-center font-black text-xl'>{name}</h4>
+             <p className='font-medium mx-auto'>"" {review.slice(0,178)}""</p>
+            <h4 className='mt-2 mb-3 text-center'> {ratings} <FaStar className='text-yellow-400 mx-auto'/></h4>
            </div>
          
         </div>
