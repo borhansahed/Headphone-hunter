b20517dd5390c39b05849a021e92bcf425c2e6d1
wahi90 <mdshahed1098@gmail.com>
2022-04-05 15:41:27 +0600

    bouns questions and answer done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './About.css'
+
 
 const About = () => {
     return (
-        <div>
-            <h1>MOHAMMAD SAHED</h1>
+        <div className='about-container'>
+            <h1 className='text-4xl font-black mt-12 text-center'>MOHAMMAD SAHED</h1>
+            <p className='text-2xl font-black mt-5 text-center'>New Web Developer is under processing </p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
+        <div className='blogs-container' >
+            <h1 className='text-4xl font-black text-center mt-10'> ! WHAT IS CONTEXT API ?</h1>
+            <p>Context Api is opposite of props drilling ! Context Api can pass any data from anywhere ! Context Api is very useful when we have many component but we need to give data in one spcific component !</p>
+            <h1 className='text-4xl font-black text-center mt-10'> !! WHAT SEMANTIC TAG ?</h1>
+            <p>Semantic tag is  Introduced in Html5 ! Semantic tag defined the content of website ! Ex: !header! anyone can will be understand this the website header! </p>
         </div>
     );
 };
