f7b59069a9eb3346686b9a7474390e77da1874ac
wahi90 <mdshahed1098@gmail.com>
2022-06-05 15:52:56 +0600

    change repo private to public

--- src/components/Navber/Navber.js
+++ src/components/Navber/Navber.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navber.css'
+
 const Navber = () => {
     return (
         <div className='nav-container'>
           <div className='nav-style'>
-            <Link to="/home">HOME</Link>
+            
+
+                <Link to="/home">HOME</Link>
             <Link to="/review">REVIEW</Link>
             <Link to="/dashboard">DASHBOARD</Link>
             <Link to="/blogs">BLOGS</Link>
             <Link to="/about">ABOUT</Link>
+            
+          
           </div>
         </div>
     );
